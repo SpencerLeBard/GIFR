9446a7de5ce67446157302cb821505448ba5b08d
SpencerLeBard <slebard@gmail.com>
2020-09-11 14:21:31 -0600

    models

--- client/app/Models/Post.js
+++ client/app/Models/Post.js
@@ -1,10 +1,33 @@
 export default class Post {
-  constructor({ title, authorImg, author, body, tags, creatorEmail }) {
+  constructor({ title, authorImg, author, body, tags, creatorEmail, id }) {
     this.title = title
-    this.authorImg = authorImg
+    this.authorImg = authorImg || "https://placehold.it/100x100"
     this.author = author
     this.body = body
     this.tags = tags
     this.creatorEmail = creatorEmail
+    this.id = id
+  }
+  get postTemplate() {
+
+    return `
+  <div class="row">
+  <div class="col-3 card">
+      <div class="card-img p-2" id="avatar"> <img src="${this.authorImg}" alt="">
+          <div class="d-flex align-items-center" id="usernameArea"> ${this.author}
+          </div>
+          <div class="d-flex align-items-center" id="usernameArea"> score
+          </div>
+      </div>
+  </div>
+  <div class="col-9 card">
+      <div class="card-title p-2">title
+          <div class="card-body border-top"> 
+          ${this.body}
+          </div>
+      </div>
+      <!-- ${this.commentTemplate} -->*//
+  </div>
+</div>`
   }
 }
---
+++ client/app/Services/PostService.js
@@ -0,0 +1,10 @@
+import { ProxyState } from "../AppState.js";
+import Post from "../Models/Post.js";
+
+class PostService {
+  addValue() {
+
+  }
+}
+
+export const valuesService = new PostService();
