d3aaa942e2b74d1411af266b471b60a544c3410f
SpencerLeBard <slebard@gmail.com>
2020-09-11 10:52:58 -0600

    fixed folders

---
+++ client/app/AuthConfig.js
@@ -0,0 +1,3 @@
+export const domain = 'dev-tunnelvision77.us.auth0.com'
+export const audience = 'https://bad-advice.com/'
+export const clientId = '7UuCA6kMzKde5MTSXk6CZpsX0aFNDIoZ'
--- gifr/client/app/AuthConfig.js
+++
@@ -1,3 +0,0 @@
-export const domain = 'http://dev-tunnelvision77.us.auth0.com/'
-export const audience = 'https://bad-advice.com/'
-export const clientId = 'ODwEKGnjmVVfKf69s1xh86SyWnavXZ2H'
