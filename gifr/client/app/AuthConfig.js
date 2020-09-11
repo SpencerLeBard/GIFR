c7dac017c6ba8c849ea775b1b01956489965dec2
SpencerLeBard <slebard@gmail.com>
2020-09-11 10:28:50 -0600

    inin

--- gifr/client/app/AuthConfig.js
+++ gifr/client/app/AuthConfig.js
@@ -1,3 +1,3 @@
-export const domain = ''
-export const audience = ''
-export const clientId = ''
+export const domain = 'http://dev-tunnelvision77.us.auth0.com/'
+export const audience = 'https://bad-advice.com/'
+export const clientId = 'ODwEKGnjmVVfKf69s1xh86SyWnavXZ2H'
